0a950efe6e1692e2e4dcc7bf21c08fac29f4f67c
yk <kang.yu@gemii.cc>
2017-10-21 16:17:26 +0800

    add swagger

--- yk-api/yk-demo-api/src/main/java/org/yk/demo/api/controller/DemoConroller.java
+++ yk-api/yk-demo-api/src/main/java/org/yk/demo/api/controller/DemoConroller.java
@@ -6,10 +6,13 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 import org.yk.demo.api.data.DemoData;
 
+import io.swagger.annotations.ApiOperation;
+
 @RestController
 public class DemoConroller {
 
 	@RequestMapping(value="/demo/demoData",method = RequestMethod.POST)
+	@ApiOperation(value = "测试接口", notes = "测试接口")
 	public DemoData test(@RequestBody DemoData demoData){
 		return demoData;
 	}
--- yk-api/yk-demo-api/src/main/java/org/yk/demo/api/data/DemoData.java
+++ yk-api/yk-demo-api/src/main/java/org/yk/demo/api/data/DemoData.java
@@ -2,9 +2,12 @@ package org.yk.demo.api.data;
 
 import java.io.Serializable;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 @Data
+@ApiModel(description = "测试类")
 public class DemoData implements Serializable{
 
 	/**
@@ -12,7 +15,9 @@ public class DemoData implements Serializable{
 	 */
 	private static final long serialVersionUID = 4527129367544644002L;
 	
+	@ApiModelProperty(notes = "id")
 	private String id;
+	@ApiModelProperty(notes = "name")
 	private String name;
 	
 }
