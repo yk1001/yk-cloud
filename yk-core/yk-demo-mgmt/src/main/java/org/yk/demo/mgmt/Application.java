b60982e8fee0b36227daf9448a934bbcd5aeb41f
yk <kang.yu@gemii.cc>
2017-10-31 20:56:09 +0800

    demo mgmt

---
+++ yk-core/yk-demo-mgmt/src/main/java/org/yk/demo/mgmt/Application.java
@@ -0,0 +1,21 @@
+package org.yk.demo.mgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+
+/**
+ * Hello world!
+ *
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@EnableResourceServer
+public class Application
+{
+    	public static void main( String[] args )
+        {
+        	SpringApplication.run(Application.class, args);
+        }
+}
