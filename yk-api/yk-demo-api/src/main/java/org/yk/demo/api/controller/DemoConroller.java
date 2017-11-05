1b4f0d98bfd8a2d829960b9f1aa5ea2414b5f1a9
yk <kang.yu@gemii.cc>
2017-11-05 20:20:18 +0800

    commit

--- yk-api/yk-demo-api/src/main/java/org/yk/demo/api/controller/DemoConroller.java
+++ yk-api/yk-demo-api/src/main/java/org/yk/demo/api/controller/DemoConroller.java
@@ -1,6 +1,15 @@
 package org.yk.demo.api.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
+ß
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+ß
+import javax.servlet.ServletOutputStream;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.bind.annotation.RequestAttribute;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -26,5 +35,30 @@ public class DemoConroller {
 	@ApiOperation(value = "测试接口", notes = "测试接口")
 	public DemoData test2(@RequestBody DemoData demoData){
 		return demoMgmtClient.test2(demoData);
+	}
+	
+	@RequestMapping(value="/noauth/download",method = RequestMethod.GET)
+	@ApiOperation(value = "服务器文件下载", notes = "服务器文件下载")
+	public void download(@RequestAttribute Download download,HttpServletResponse resp){
+		try {
+			String filename = download.getPath();
+			resp.setHeader("Content-Disposition", "attachment;filename="+filename);  
+	        //System.out.println(fullFileName);  
+	        //读取文件  
+	        InputStream in = new FileInputStream(filename);  
+	        OutputStream out = resp.getOutputStream();  
+	        
+	        int b;  
+	        while((b=in.read())!= -1)  
+	        {  
+	            out.write(b);  
+	        }  
+	          
+	        in.close();  
+	        out.close();  
+	        
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 }
