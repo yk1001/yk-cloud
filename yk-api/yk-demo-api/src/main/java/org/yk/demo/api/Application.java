754bcfd20f98e007a3c34890d315e33a425f1303
Administrator <Administrator@D5SQV2K2ASI1CSU>
2018-10-14 22:44:19 +0800

    modify yk-demo-api code

--- yk-api/yk-demo-api/src/main/java/org/yk/demo/api/Application.java
+++ yk-api/yk-demo-api/src/main/java/org/yk/demo/api/Application.java
@@ -5,23 +5,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
-/**
- * Hello world!
- *
- */
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableResourceServer
 @EnableHystrix
 @EnableFeignClients(basePackages = {"org.yk.demo.api.client"})
-@ComponentScan(basePackages = {"org.yk"})
-public class Application
-{
-    	public static void main( String[] args )
-        {
+public class Application{
+	
+    	public static void main( String[] args ){
         	SpringApplication.run(Application.class, args);
         }
 }
