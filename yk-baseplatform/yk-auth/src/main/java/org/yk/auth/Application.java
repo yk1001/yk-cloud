cd590d55e20322abb369a611353a0e941dca73e5
Administrator <Administrator@D5SQV2K2ASI1CSU>
2018-10-14 22:27:17 +0800

    modify auth code

--- yk-baseplatform/yk-auth/src/main/java/org/yk/auth/Application.java
+++ yk-baseplatform/yk-auth/src/main/java/org/yk/auth/Application.java
@@ -2,6 +2,7 @@ package org.yk.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
@@ -12,7 +13,7 @@ import org.springframework.security.oauth2.config.annotation.web.configuration.E
  */
 @SpringBootApplication
 @EnableAuthorizationServer
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 public class Application {
 	
     public static void main( String[] args ){
