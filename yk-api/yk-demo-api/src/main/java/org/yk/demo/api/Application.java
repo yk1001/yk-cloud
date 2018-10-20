70e7a5d9435b7dc1140cef55cc7deaf65810f16d
Administrator <Administrator@D5SQV2K2ASI1CSU>
2018-10-20 20:20:04 +0800

    add resource server config

--- yk-api/yk-demo-api/src/main/java/org/yk/demo/api/Application.java
+++ yk-api/yk-demo-api/src/main/java/org/yk/demo/api/Application.java
@@ -5,16 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
-import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-@EnableResourceServer
 @EnableHystrix
 @EnableFeignClients(basePackages = {"org.yk.demo.api.client"})
 public class Application{
 	
-    	public static void main( String[] args ){
-        	SpringApplication.run(Application.class, args);
-        }
+	public static void main( String[] args ){
+    	SpringApplication.run(Application.class, args);
+    }
 }
--- yk-api/yk-demo-api/src/main/java/org/yk/demo/api/controller/DemoConroller.java
+++ yk-api/yk-demo-api/src/main/java/org/yk/demo/api/controller/DemoConroller.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+import org.yk.common.constant.ResultCode;
+import org.yk.common.data.GeneralContentResult;
 import org.yk.common.data.demomgmt.DemoData;
 import org.yk.demo.api.client.DemoMgmtClient;
 
@@ -20,11 +22,19 @@ public class DemoConroller {
 	@Autowired
 	private DemoMgmtClient demoMgmtClient;
 	
-	@RequestMapping(value="/noauth/demoData",method = RequestMethod.POST)
+	@RequestMapping(value="/noauth/demoData",method = RequestMethod.GET)
 	@ApiOperation(value = "测试接口", notes = "测试接口")
-	public DemoData test(@RequestBody DemoData demoData){
-		log.info("接收到参数：{}",JSONObject.toJSONString(demoData));
-		return demoMgmtClient.test(demoData);
+	public GeneralContentResult<DemoData> test(){
+		log.info("接收到参数");
+		// mock data
+		DemoData demoData = new DemoData();
+		demoData.setId("1");
+		demoData.setName("test");
+		GeneralContentResult<DemoData> result = new GeneralContentResult<DemoData>();
+		result.setResultCode(ResultCode.OPERATE_SUCCESS);
+		result.setDetailDescription("success");
+		result.setResultContent(demoData);
+		return result;
 	}
 	
 	@RequestMapping(value="/authsec/demoData",method = RequestMethod.POST)
