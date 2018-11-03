cc3208c6d0087ceed22e8f027d7e389b30ae907b
Administrator <Administrator@D5SQV2K2ASI1CSU>
2018-11-03 18:14:45 +0800

    add scan package

--- yk-api/yk-demo-api/src/main/java/org/yk/demo/api/Application.java
+++ yk-api/yk-demo-api/src/main/java/org/yk/demo/api/Application.java
@@ -5,11 +5,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableHystrix
 @EnableFeignClients(basePackages = { "org.yk.demo.api.client" })
+@ComponentScan(value={"org.yk.common.security","org.yk.demo.api"})
 public class Application {
 
 	public static void main(String[] args) {
