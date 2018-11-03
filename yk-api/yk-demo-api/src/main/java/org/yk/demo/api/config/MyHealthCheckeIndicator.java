c38b9cd5382eadb429bd4b6351c8d7e57ef72dbe
Administrator <Administrator@D5SQV2K2ASI1CSU>
2018-11-03 16:51:35 +0800

    optimise graceful shutdown

--- yk-api/yk-demo-api/src/main/java/org/yk/demo/api/config/MyHealthCheckeIndicator.java
+++ yk-api/yk-demo-api/src/main/java/org/yk/demo/api/config/MyHealthCheckeIndicator.java
@@ -1,13 +1,19 @@
 package org.yk.demo.api.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.actuate.health.Health;
 import org.springframework.boot.actuate.health.HealthIndicator;
 import org.springframework.stereotype.Component;
+
+import com.netflix.discovery.EurekaClient;
 
 @Component
 public class MyHealthCheckeIndicator implements HealthIndicator{
 	
 	private boolean up = true;
+	
+	@Autowired
+	private EurekaClient eurekaClient;
 	
     @Override
     public Health health() {
@@ -24,6 +30,9 @@ public class MyHealthCheckeIndicator implements HealthIndicator{
     }
  
     public void setUp(boolean up) {
+    	if(!up){
+    		eurekaClient.shutdown();
+    	}
         this.up = up;
     }
 }
