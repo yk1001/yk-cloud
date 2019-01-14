00e48a7f438e446f5e28a162c84c86ef22999727
Administrator <Administrator@D5SQV2K2ASI1CSU>
2019-01-14 20:29:00 +0800

    swagger add authsec header input

--- yk-api/yk-demo-api/src/main/java/org/yk/demo/api/controller/DemoConroller.java
+++ yk-api/yk-demo-api/src/main/java/org/yk/demo/api/controller/DemoConroller.java
@@ -1,7 +1,6 @@
 package org.yk.demo.api.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,8 +9,8 @@ import org.yk.common.data.GeneralContentResult;
 import org.yk.common.data.demomgmt.DemoData;
 import org.yk.demo.api.client.DemoMgmtClient;
 
-import com.alibaba.fastjson.JSONObject;
-
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 
@@ -45,6 +44,8 @@ public class DemoConroller {
 	
 	@RequestMapping(value="/authsec/demoData",method = RequestMethod.GET)
 	@ApiOperation(value = "测试接口", notes = "测试接口")
+    @ApiImplicitParams({
+            @ApiImplicitParam(paramType = "header", name = "Authorization", dataType = "String", required = true, value = "Token", defaultValue = "bearer") })
 	public GeneralContentResult<DemoData> test2(){
 		log.info("接收到参数");
 		try {
