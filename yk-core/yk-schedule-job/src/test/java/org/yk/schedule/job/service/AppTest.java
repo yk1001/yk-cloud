70921b97138a677edb8e0407cf4ef38b1c89ad13
Administrator <Administrator@D5SQV2K2ASI1CSU>
2019-02-18 23:12:51 +0800

    add schedule job

---
+++ yk-core/yk-schedule-job/src/test/java/org/yk/schedule/job/service/AppTest.java
@@ -0,0 +1,12 @@
+package org.yk.schedule.job.service;
+
+import org.junit.Test;
+import org.yk.schedule.job.AbstractTest;
+
+public class AppTest extends AbstractTest{
+
+	@Test
+	public void test(){
+		System.out.println(1);
+	}
+}
