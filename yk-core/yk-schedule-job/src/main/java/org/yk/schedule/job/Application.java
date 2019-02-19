5c65f745800fc6c57eefb25e2b1d3245dc328030
Administrator <Administrator@D5SQV2K2ASI1CSU>
2019-02-20 00:33:43 +0800

    add mysql sql file

--- yk-core/yk-schedule-job/src/main/java/org/yk/schedule/job/Application.java
+++ yk-core/yk-schedule-job/src/main/java/org/yk/schedule/job/Application.java
@@ -2,12 +2,15 @@ package org.yk.schedule.job;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+@EnableDiscoveryClient(autoRegister = false)
 public class Application {
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
+	
 }
